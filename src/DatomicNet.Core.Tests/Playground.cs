c08663908227e35a515b40e856d43fee203fa333
forestjohnsonilm <forest.johnson@ilmservice.com>
2016-03-14 20:29:10 -0500

    compile proto and added test for serializer

---
+++ src/DatomicNet.Core.Tests/Playground.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using DatomicNet.Core;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+
+namespace DatomicNet.Core.Tests
+{
+    public class Playground
+    {
+
+        [Fact]
+        public void TestDictionaryBehavesAsExpected()
+        {
+            var t = new List<KeyValuePair<int, string>> { new KeyValuePair<int, string>(1, "a"), new KeyValuePair<int, string>(1, "b") };
+            Action a = () => t.ToDictionary(x => x.Key, x => x.Value);
+            a.ShouldThrow<ArgumentException>();
+
+            var t1 = new List<KeyValuePair<int, string>> { new KeyValuePair<int, string>(2, "a"), new KeyValuePair<int, string>(1, "b") };
+            Action a1 = () => t1.ToDictionary(x => x.Key, x => x.Value);
+            a1.ShouldNotThrow<Exception>();
+        }
+
+    }
+}
---
+++ src/DatomicNet.Core.Tests/ProtobufSerializerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using DatomicNet.Core;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Google.Protobuf;
+
+namespace DatomicNet.Core.Tests
+{
+    public class ProtobufSerializerTest
+    {
+
+        [Fact]
+        public void SerializerSmokeTest()
+        {
+            var assemblies = new Assembly[] { typeof(ProtobufSerializerTest).GetTypeInfo().Assembly };
+            var typeRegistry = new TypeRegistry((type) => typeof(IMessage).IsAssignableFrom(type), assemblies);
+            var datomSerializer = new ProtobufDatomSerializer(typeRegistry, assemblies);
+        }
+        
+
+    }
+
+    public class InitialSchema : ISchemaChange
+    {
+        public IReadOnlyDictionary<uint, Func<IEnumerable<Datom>, IEnumerable<Datom>>> MapEntityStreamForType => null;
+
+        public IReadOnlyDictionary<Type, uint> RegisterAggregates => new Dictionary<Type, uint>()
+        {
+            { typeof(TestAggregate), 1 },
+        };
+
+        public IReadOnlyDictionary<Type, uint> RegisterTypes => new Dictionary<Type, uint>()
+        {
+            { typeof(TestAggregate), 1 },
+            { typeof(TransactionCategory), 2 },
+            { typeof(OtherThing), 3 }
+        };
+
+        public bool RequiresReIndex => false;
+        public long TransactionId => 1;
+    }
+
+}
